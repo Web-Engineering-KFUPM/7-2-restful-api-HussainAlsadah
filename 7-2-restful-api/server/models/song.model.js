36e5213745170d1d5821b37d0365629fcb1f27af
HussainAlsadah <s202282700@kfupm.edu.sa>
2026-04-15 10:38:10 +0300

    initial fix attempt

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -3,3 +3,15 @@ import mongoose from "mongoose";
 // db schema
 
 export const Song = mongoose.model("Song", songSchema);
+const songSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    artist: { type: String, required: true, trim: true },
+    year: { type: Number }
+  },
+  { timestamps: true }
+);
+
+const Song = mongoose.model("Song", songSchema);
+
+export { Song };
