0c5130f70b90665ad230b7f02d334c4ffc11daa5
HussainAlsadah <s202282700@kfupm.edu.sa>
2026-04-15 10:36:06 +0300

    did the last todo

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -53,4 +53,8 @@ app.put("/api/songs/:id", async (req, res) => {
 });
 // /api/songs/:id (Delete song)
 
-app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
+app.delete("/api/songs/:id", async (req, res) => {
+  const deleted = await Song.findByIdAndDelete(req.params.id);
+  if (!deleted) return res.status(404).json({ message: "Song not found" });
+  res.status(204).end();
+});
